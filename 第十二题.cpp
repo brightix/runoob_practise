cb9d3260339d9b7e887a67934b483d5376920ab5
刘渔 <162892491+brightix@users.noreply.github.com>
2024-06-23 22:52:55 +0800

    Add files via upload

---
+++ 第十二题.cpp
@@ -0,0 +1,23 @@
+﻿//No.12   判断 101 到 200 之间的素数。
+
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	printf("101到200的素数有\n");
+	for (i = 101; i <= 200; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+			if (i % j != 0 && j == i - 1)
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+	return 0;
+}
